7e84f86370075e19b278310eff237a20f067c78b
abbas <abbas.oveissi@gmail.com>
2014-03-18 16:17:07 +0330

    create new structure for kernel's files

---
+++ system.h
@@ -0,0 +1,24 @@
+
+
+
+/* This defines what the stack looks like after an ISR was running */
+struct regs
+{
+    unsigned int gs, fs, es, ds;      /* pushed the segs last */
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* pushed by 'pusha' */
+    unsigned int int_no, err_code;    /* our 'push byte #' and ecodes do this */
+    unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */ 
+};
+       
+
+
+extern void irq_install_handler(int irq, void (*handler)(struct regs *r));
+extern void irq_uninstall_handler(int irq);
+extern void irq_remap(void);
+extern void irq_install();
+extern void irq_handler(struct regs *r);
+ 
+
+extern unsigned char keyboard_read_scanCode();
+extern void terminal_writestring(const char* data);
+extern void terminal_initialize();
