a063bebd8e6cae1b2db53dcbe60f28efac58839a
Abbas Oviessi <abbas.oveissi@gmail.com>
2014-04-17 19:51:13 +0430

    hazfe bakhsh haye ezafe

--- includes/system.h
+++ includes/system.h
@@ -1,10 +1,6 @@
-typedef unsigned int u32_t;
-
-
 #define MAX_INTERRUPTS ( 256 )
 #define GDT_DESCRIPTORS_NO ( 3 )
 #define SYS_CALL_INTERRUPT ( 80 )
-
 
 typedef struct registers
 {
@@ -14,10 +10,8 @@ typedef struct registers
    	u32_t eip, cs, eflags, useresp, ss;	// Pushed by the processor automatically.
 } registers_t;
 
-
-
 typedef void (*isr_t)(registers_t);
-       
+
 extern unsigned char inportb (unsigned short _port);
 extern void outportb (unsigned short _port, unsigned char _data);
 
@@ -26,7 +20,6 @@ extern void irq_uninstall_handler(int irq);
 extern void irq_remap(void);
 extern void irq_install();
  
-
 extern unsigned char keyboard_read_scanCode();
 extern void terminal_writestring(const char* data);
 extern void terminal_initialize();
@@ -35,12 +28,9 @@ extern void gdt_init(void);
 extern void idt_init(void);
 extern void pic_init(void);
 
-
-
 extern char* ptrToStr(unsigned int ptr,int base);
 extern char * intToStr(unsigned int ptr);
 extern void terminal_putchar(char c);
 extern void backSpaceKey();
-
 
 extern void register_isr( unsigned char n, isr_t handler );
